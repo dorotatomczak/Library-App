7e39d30f4608ff4be6e5255274f142d24dafefe2
Dorota <tomczakd65@gmail.com>
2017-07-05 01:09:06 +0200

    Added basic registration functionality

--- Library/Library/ViewModels/RegisterViewModel.cs
+++ Library/Library/ViewModels/RegisterViewModel.cs
@@ -1,7 +1,113 @@
-﻿
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Windows;
+using System.Windows.Input;
+
 namespace Library
 {
-    class RegisterViewModel
+    class RegisterViewModel : ViewModelBase
     {
+        private string _PasswordInVM;
+        private string _username;
+        private string _email;
+
+        public RegisterViewModel()
+        {
+            SubmitCommand = new RelayCommand(Submit);
+        }
+
+        public ICommand SubmitCommand
+        {
+            get;
+            private set;
+        }
+
+        public string UserName
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+
+        public string PasswordInVM
+        {
+            get
+            {
+                return _PasswordInVM;
+            }
+            set
+            {
+                _PasswordInVM = value;
+                OnPropertyChanged("PasswordInVM");
+            }
+        }
+
+        private void Submit(object parameter)
+        {
+            var passwordContainer = parameter as IHavePassword;
+            if (passwordContainer != null)
+            {
+                var secureString = passwordContainer.Password;
+                PasswordInVM = ConvertToUnsecureString(secureString);
+            }
+
+            SqlConnection connection = new SqlConnection(@"Data Source=localhost\SQLEXPRESS; Initial Catalog=LibraryDB; Integrated Security=True;");
+
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                    connection.Open();
+
+                ///TO DO: dodanie do bazy danych
+                string query = "insert into user_tbl (Login, Email, Password) values('"+this.UserName+"', '"+this.Email+"', '"+this.PasswordInVM+"')";
+
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Saved");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private string ConvertToUnsecureString(SecureString securePassword)
+        {
+            if (securePassword == null)
+            {
+                return string.Empty;
+            }
+
+            IntPtr unmanagedString = IntPtr.Zero;
+            try
+            {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
+                return Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
     }
 }
--- Library/Library/Views/RegisterView.xaml.cs
+++ Library/Library/Views/RegisterView.xaml.cs
@@ -6,11 +6,20 @@ namespace Library
     /// <summary>
     /// Interaction logic for RegisterView.xaml
     /// </summary>
-    public partial class RegisterView : UserControl
+	public partial class RegisterView : UserControl, IHavePassword
     {
         public RegisterView()
         {
             InitializeComponent();
+            DataContext = new RegisterViewModel();
+        }
+
+        public System.Security.SecureString Password
+        {
+            get
+            {
+                return txtPassword.SecurePassword;
+            }
         }
     }
 }
